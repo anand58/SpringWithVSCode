5bc5da3b64c148609e2a54bd1dce7e338bcd5b1e
Anand Tiwari <anandt.tiwari12@gmail.com>
2020-02-23 02:50:53 +0530

    first commit

---
+++ src/test/java/com/springbootonvisualcode/springbootonvisualcodedemo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootonvisualcode.springbootonvisualcodedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
